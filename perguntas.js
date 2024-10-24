5fc269c00c9773370a77f5fc76584ebd7d3411e2
Bruno Custódio Correia <brunocustodiocorreia9@gmail.com>
2024-10-24 11:42:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'O que é o X na matemática?',
+    'O X na matemática é uma incógnita.'
+)
+
+criaCartao(
+    'Filosofia',
+    'Quem é o autor da frase "Corgito Ergo Sum"?',
+    'O autor dessa frase é René Descartes'
+)
+
+criaCartao(
+    'História',
+    'Quando foi o fim da Segunda Guerra Mundial?',
+    'O fim da Segunda Guerra Mundial foi em 1945 e seu inicio em 1939.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que significa BTW na gíria dos Estados Unidos?',
+    'Significa By The Way, pode ser traduzido como: "por falar nisso".'
+)
